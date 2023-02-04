6c476a71fbdaf13e205d8880719dfaf4d088623d
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2023-02-04 12:16:48 +0530

    added routes

--- client/.history/src/App_20230204120510.js
+++ client/.history/src/App_20230204120510.js
@@ -1,7 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import homepage from "./pages/homepage/homepage";
 
 function App() {
   return (
---
+++ client/.history/src/pages/homepage/homepage_20230204120850.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const homepage = () => {
+  return <div className="homepage">homepage</div>;
+};
+
+export default homepage;
