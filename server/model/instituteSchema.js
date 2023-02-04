d73dafb740984d1b3d94daf6ccbea3cf5006d128
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2023-02-04 12:12:16 +0530

    Login and Register routes for Volunteer and Institute

---
+++ server/model/instituteSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const instituteSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+});
+
+instituteSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = bcrypt.hashSync(this.password, 12);
+    }
+    next();
+});
+
+instituteSchema.methods.generateAuthToken = async function () {
+    try {
+        let token= jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const institute = mongoose.model('Institute', instituteSchema);
+
+module.exports = institute;
