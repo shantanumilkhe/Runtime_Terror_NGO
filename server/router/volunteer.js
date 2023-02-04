d3f26c87d9c95c48e7bed18514318f86ebbcde42
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2023-02-04 15:10:54 +0530

    Added routes for admin

--- server/router/volunteer.js
+++ server/router/volunteer.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
+const Volunteer = require('../model/volunteerSchema');
 
+router.get('/getassignedstudents',passport.authenticate('jwt', { session: false }),async(req,res)=>{
+    let vol = req.user;
+    let volunteer = await volunteer ({_id:vol});
+    console.log(volunteer.assignedStudents);
+})
 
 module.exports = router;
