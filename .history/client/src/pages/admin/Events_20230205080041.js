f83128526f10989f2c6332c91b866d63e40ad965
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2023-02-05 22:42:39 +0530

    LOR generation check

--- .history/client/src/pages/admin/Events_20230205080041.js
+++ .history/client/src/pages/admin/Events_20230205080041.js
@@ -8,7 +8,16 @@ const Events = () => {
   const handleClick = async (id) => {
     console.log(id);
     const token = localStorage.getItem("token");
-
+    await fetch("http://localhost:5000/vol/participate/" + id, {
+      method: "POST",
+      headers: { Authorization: token },
+    })
+      .then((res) => {
+        if (res.status === 200) console.log("participated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     //   axios.post('http://localhost:5000/vol/participate/'+id,{headers:{'Authorization':token}}).then((res)=>{
     //     console.log('participated')
     //   }).catch((err)=>{
@@ -57,6 +66,9 @@ const Events = () => {
               <th scope="col" class="px-6 py-3">
                 description
               </th>
+              <th scope="col" class="px-6 py-3">
+                Action
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -77,6 +89,16 @@ const Events = () => {
                         event.volunteersAssigned.length}
                     </td>
                     <td class="px-6 py-4">{event.description}</td>
+                    <td class="px-6 py-4">
+                      <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                          handleClick(event._id);
+                        }}
+                      >
+                        Participate
+                      </button>
+                    </td>
                   </tr>
                 ))
               : null}
